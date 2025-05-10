6ce4ef2f80df2cfe63ed9f4edc30d74232c15e71
Agus Miftachul <221111031@mhs.stiki.ac.id>
2025-05-10 14:34:15 +0700

    Update

---
+++ Belajar_12/all_numbers.py
@@ -0,0 +1,12 @@
+# Sample list of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Initialize a variable to store the sum
+product_of_numbers = 1
+
+# Iterate through the list and accumulate the sum
+for i in numbers:
+    product_of_numbers *= i
+    
+# Print the sum
+print("Product of elements in the list:", product_of_numbers)
